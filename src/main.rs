baf7cb4aa11fa6eb4de9913a9fa414c9b1727ffa
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2022-12-09 19:26:38 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::str::FromStr;
+use std::env;
+use slidy::puzzle::{
+    color_scheme::Scheme, coloring::RainbowBrightFull, label::label::SplitFringe, puzzle::Puzzle,
+    render::{Renderer, Font, Text},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<_> = env::args().collect();
+    //if args.len() == 0 {
+    //    Ok(())
+    //}
+    let p = Puzzle::from_str(&args[1])?;
+    let svg = Renderer::new()
+        .scheme(Scheme::new(
+            Box::new(SplitFringe),
+            Box::new(RainbowBrightFull)
+        ))
+        .text(Text::new())
+        .svg(&p)?;
+    svg::save("out.svg", &svg)?;
+    Ok(())
+}
