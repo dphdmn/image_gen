8233ed9213bc46938ee343643ad85be84fb71c74
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2022-12-10 02:09:13 +0800

    changable color scheme
    Co-authored-by: benwh1 <benwh1@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,73 @@
 use std::str::FromStr;
-use std::env;
 use slidy::puzzle::{
-    color_scheme::Scheme, coloring::RainbowBrightFull, label::label::SplitFringe, puzzle::Puzzle,
+    color_scheme::Scheme, coloring::RainbowBright, label::{label::{Label, SplitFringe, Rows}, rect_partition::{Rect, RectPartition}}, puzzle::Puzzle,
     render::{Renderer, Text},
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<_> = env::args().collect();
-    if args.len() == 1 {
-        println!("Input scramble at least!");
-        return Ok(());
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    state: String,
+
+    #[arg(short, long)]
+    label: String,
+}
+
+macro_rules! make_label {
+    ($arg: expr, $($label: expr),+) => {
+        match $arg {
+            $(stringify!($label) => return Some(Box::new($label)),)+
+            _ => {},
+        }
     }
-    let p = Puzzle::from_str(&args[1])?;
+}
+
+fn parse_label(arg: &str) -> Option<Box<dyn Label>> {
+    make_label!(arg, Rows, SplitFringe);
+
+    match arg {
+        "10x10" => Some(Box::new(
+            RectPartition::new(
+                vec![
+                    Rect::new((0,0), (5,5)).unwrap(),
+                    Rect::new((5,0), (10,5)).unwrap(),
+                    Rect::new((0,5), (5,10)).unwrap(),
+                    Rect::new((5,5), (10,10)).unwrap(),
+                ]
+            ).unwrap()
+        )),
+        _ => None,
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let grids = Box::new(
+    //     RectPartition::new(
+    //         vec![
+    //             Rect::new((0,0), (5,5))?,
+    //             Rect::new((5,0), (10,5))?,
+    //             Rect::new((0,5), (5,10))?,
+    //             Rect::new((5,5), (10,10))?,
+    //         ]
+    //     )?
+    // );
+
+    let args = Args::parse();
+
+    let p = Puzzle::from_str(&args.state)?;
+    let label = parse_label(&args.label).expect("Invalid label");
+
     let svg = Renderer::new()
         .scheme(Scheme::new(
-            Box::new(SplitFringe),
-            Box::new(RainbowBrightFull)
+            label,
+            Box::new(RainbowBright)
         ))
         .text(Text::new())
         .svg(&p)?;
+
     svg::save("out.svg", &svg)?;
+
     Ok(())
 }
