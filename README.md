1c274c5937d7793433f5c8a959e6b9a8099b7458
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2022-12-31 18:50:11 +0800

    test

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
-# image_gen
+Version: 2.0 beta
 
-Generates an SVG image of 15 puzzle or any other size sliding puzzle.
+Usage: image_gen.exe [OPTIONS]
 
-Usage:
+Options:
+-s, --state [default: "1 2 3 4/5 6 7 8/9 10 11 12/13 14 15 0"]
+-l, --label [default: SplitFringe]
+-c, --color [default: RainbowBright]
+-n, --notext
+-b, --border
+-f, --fontsize [default: 30]
+-h, --help Print help information
 
-image_gen.exe "14 0 7 6/8 4 2 10/13 12 5 15/9 1 11 3"
+Labels supported atm:
+Trivial,
+RowGrids,
+Rows,
+Fringe,
+FringeGrids,
+SquareFringe,
+SplitFringe,
+SplitSquareFringe,
+Diagonals,
+LastTwoRows,
+SplitLastTwoRows,
+ConcentricRectangles,
+Spiral,
+SpiralGrids
+
+Colors supported atm:
+Rainbow,
+RainbowFull,
+RainbowBright,
+RainbowBrightFull
+
+Complex input example:
+image_gen.exe --state "2 4 13 17 9/8 5 3 19 16/21 15 24 0 1/7 10 23 11 18/20 22 6 14 12" -l "Rows" --color "RainbowBrightFull" -b
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::str::FromStr;
 use slidy::puzzle::{
-    color_scheme::Scheme, coloring::{Coloring, Rainbow, RainbowFull, RainbowBright, RainbowBrightFull}, label::{label::{Label, Trivial, RowGrids, Rows, Fringe, FringeGrids, SquareFringe, SplitFringe, SplitSquareFringe, Diagonals, LastTwoRows, SplitLastTwoRows, ConcentricRectangles, Spiral, SpiralGrids}, rect_partition::{Rect, RectPartition}}, puzzle::Puzzle,
-    render::{Renderer, Text, Borders},
+    color_scheme::Scheme,
+    coloring::{Coloring, Rainbow, RainbowBright, RainbowBrightFull, RainbowFull},
+    label::{
+        label::{
+            ConcentricRectangles, Diagonals, Fringe, FringeGrids, Label, LastTwoRows, RowGrids,
+            Rows, Spiral, SpiralGrids, SplitFringe, SplitLastTwoRows, SplitSquareFringe,
+            SquareFringe, Trivial,
+        },
+        rect_partition::{Rect, RectPartition},
+    },
+    puzzle::Puzzle,
+    render::{Borders, Renderer, Text},
 };
+use std::str::FromStr;
 
-use clap::Parser;
+use clap::{command, Arg, Parser};
 
 #[derive(Parser)]
 struct Args {
     #[arg(short, long, default_value_t = String::from("1 2 3 4/5 6 7 8/9 10 11 12/13 14 15 0"))]
     state: String,
-//test
+
     #[arg(short, long, default_value_t = String::from("SplitFringe"))]
     label: String,
 
@@ -23,7 +33,7 @@ struct Args {
     #[arg(short, long)]
     border: bool,
 
-    #[arg(short, long, default_value_t = 40.0)]
+    #[arg(short, long, default_value_t = 30.0)]
     fontsize: f32,
 }
 
@@ -36,30 +46,82 @@ macro_rules! make_label {
     }
 }
 
-fn parse_color(arg: &str) -> Option<Box<dyn Coloring>>{ //todo more color schemes....
+fn parse_color(arg: &str) -> Option<Box<dyn Coloring>> {
+    //todo more color schemes....
     make_label!(arg, Rainbow, RainbowFull, RainbowBright, RainbowBrightFull);
     None
 }
 
 fn parse_label(arg: &str) -> Option<Box<dyn Label>> {
-    make_label!(arg, Trivial, RowGrids, Rows, Fringe, FringeGrids, SquareFringe, SplitFringe, SplitSquareFringe, Diagonals, LastTwoRows, SplitLastTwoRows, ConcentricRectangles, Spiral, SpiralGrids);
-        
-        match arg {
-            "10x10" => Some(Box::new(
-                RectPartition::new(
-                    vec![
-                        Rect::new((0,0), (5,5)).unwrap(),
-                        Rect::new((5,0), (10,5)).unwrap(),
-                        Rect::new((0,5), (5,10)).unwrap(),
-                        Rect::new((5,5), (10,10)).unwrap(),
-                    ]
-                ).unwrap()
-            )),
-            _ => None,
-        }
+    make_label!(
+        arg,
+        Trivial,
+        RowGrids,
+        Rows,
+        Fringe,
+        FringeGrids,
+        SquareFringe,
+        SplitFringe,
+        SplitSquareFringe,
+        Diagonals,
+        LastTwoRows,
+        SplitLastTwoRows,
+        ConcentricRectangles,
+        Spiral,
+        SpiralGrids
+    );
+
+    match arg {
+        "10x10" => Some(Box::new(
+            RectPartition::new(vec![
+                Rect::new((0, 0), (5, 5)).unwrap(),
+                Rect::new((5, 0), (10, 5)).unwrap(),
+                Rect::new((0, 5), (5, 10)).unwrap(),
+                Rect::new((5, 5), (10, 10)).unwrap(),
+            ])
+            .unwrap(),
+        )),
+        _ => None,
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = command!()
+        .arg(
+            Arg::new("state")
+                .short('s')
+                .long("state")
+                .default_value("1 2 3 4/5 6 7 8/9 10 11 12/13 14 15 0"),
+        )
+        .arg(
+            Arg::new("label")
+                .short('l')
+                .long("label")
+                .default_values(["SplitFringe"])
+                .value_parser([
+                    "Trivial",
+                    "RowGrids",
+                    "Rows",
+                    "Fringe",
+                    "FringeGrids",
+                    "SquareFringe",
+                    "SplitFringe",
+                    "SplitSquareFringe",
+                    "Diagonals",
+                    "LastTwoRows",
+                    "SplitLastTwoRows",
+                    "ConcentricRectangles",
+                    "Spiral",
+                    "SpiralGrids",
+                ]),
+        )
+        .arg(
+            Arg::new("color")
+                .short('c')
+                .long("color")
+                .default_values(["RainbowBrightFull"]),
+        );
+
     let args = Args::parse();
     let p = Puzzle::from_str(&args.state)?;
     let label = parse_label(&args.label).expect("Invalid label");
@@ -68,20 +130,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let border = args.border;
     let fontsize = args.fontsize;
 
-    let mut renderer = Renderer::new()
-        .scheme(Scheme::new(
-            label,
-            coloring
-        ));
-    if border{
+    let mut renderer = Renderer::new().scheme(Scheme::new(label, coloring));
+    if border {
         let thickness = 1.5;
-        renderer = renderer.borders(Borders::new().thickness(thickness)).tile_gap(-thickness);
+        renderer = renderer
+            .borders(Borders::new().thickness(thickness))
+            .tile_gap(-thickness);
     }
-    if !notext{
+    if !notext {
         renderer = renderer.text(Text::new().font_size(fontsize));
     }
-    
-    let svg = renderer.svg(&p)?; 
+
+    let svg = renderer.svg(&p)?;
     svg::save("out.svg", &svg)?;
 
     Ok(())
